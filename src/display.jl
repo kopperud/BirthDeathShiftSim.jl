c0e458bbc9baab66211996b3acc79c014a172346
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-10-30 17:17:55 +0100

    nicer display

--- src/display.jl
+++ src/display.jl
@@ -1,6 +1,7 @@
 function Base.Multimedia.display(root::Root)
     n_taxa = number_of_taxa(root)
     n_extinctions = number_of_extinctions(root)
+    n_events = number_of_events(root)
     
-    println("A rooted phylogenetic tree with $n_taxa extant taxa and $n_extinctions extinction events.")
+    println("A rooted phylogenetic tree with $n_taxa extant taxa, $n_extinctions extinction events, and $n_events number of diversification rate shifts.")
 end
