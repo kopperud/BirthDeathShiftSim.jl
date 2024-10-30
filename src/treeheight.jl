a7297d6d0e71a5a744468b3d6e27d3957087dcce
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-10-30 16:53:41 +0100

    function for tree length

--- src/treeheight.jl
+++ src/treeheight.jl
@@ -11,3 +11,18 @@ end
 function treeheight(terminal::T) where {T <: TipNode}
     0.0
 end
+
+
+export treelength
+
+function treelength(node::T) where {T <: BranchingEvent}
+    h = sum(map(treelength, node.children))
+end
+
+function treelength(branch::Branch)
+    treelength(branch.outbounds) + sum(branch.times)
+end
+
+function treelength(terminal::T) where {T <: TipNode}
+    0.0
+end
